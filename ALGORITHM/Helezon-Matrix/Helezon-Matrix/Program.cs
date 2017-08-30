78d90c2810828d7b07e70c66f2e7e4d22d7362f4
Furkan Ozdemir <furkanozdmrr@hotmail.com>
2017-08-30 03:41:58 +0300

    Helezon Matrix First

---
+++ ALGORITHM/Helezon-Matrix/Helezon-Matrix/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Helezon_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int deger1 = 1,klavyedenGirilenBoyut = 1 , tasiyici = 0;
+            Console.Write("Boyut Sayisi Giriniz : ");
+            klavyedenGirilenBoyut = Convert.ToInt32(Console.ReadLine());
+            tasiyici = klavyedenGirilenBoyut;
+
+            for (int i = 0; i < klavyedenGirilenBoyut; i++)
+            {
+                for (int j = 0; j < klavyedenGirilenBoyut; j++)
+                {
+                    if (j == (klavyedenGirilenBoyut - 1))
+                    {
+                        for (int k = 0; k < klavyedenGirilenBoyut; k++)
+                        {
+                            Console.WriteLine(tasiyici + " ");
+                            tasiyici++;
+                        }
+                    }
+                    else
+                    {
+                        Console.Write(deger1 + " ");
+                        deger1++;
+
+                    }
+                }
+                Console.WriteLine("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
