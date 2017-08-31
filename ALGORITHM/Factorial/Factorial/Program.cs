7f9788dce66667fa74f61dbf84a71bd92ac889fb
Furkan Ozdemir <furkanozdmrr@hotmail.com>
2017-09-01 01:28:32 +0300

    End

---
+++ ALGORITHM/Factorial/Factorial/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.Write("Geçerli bir değer giriniz : ");
+            long factorialNumber = Convert.ToInt64(Console.ReadLine());
+            long result = 0;
+
+
+
+            if (factorialNumber > 0)
+            {
+                for (int i = 1; i <= factorialNumber; i++)
+                {
+                    result = factorialNumber * i;
+                }
+                Console.WriteLine("Faktöriyeli : " + result);
+            }
+            else
+            {
+                Console.WriteLine("Geçersiz değer....");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
